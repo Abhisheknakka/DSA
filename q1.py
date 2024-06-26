77c1f1a00b315ff7d9c471ddf5f493da4dd12b03
Abhisheknakka <abhisheknakka15@gmail.com>
2024-06-26 08:00:13 -0400

    02 update

---
+++ q1.py
@@ -0,0 +1,20 @@
+# array operations
+
+import array
+
+arr1 = array.array('i',[1,2,3])
+print(arr1)
+
+
+for i in range (0, 3):
+    print (arr1[i], end=" ")
+print("\r")
+
+ 
+# using append() to insert new value at end
+arr1.append(43);
+ 
+# printing appended array
+print("The appended array is : ", end="")
+for i in range (len(arr1)):
+    print (arr1[i], end=" ")
